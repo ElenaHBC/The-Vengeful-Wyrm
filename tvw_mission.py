63014b5b9ad7f1dbacb0b85c0fce02abb16f835d
Elena Tomeva <et@healthbiocare.at>
2024-06-13 20:15:44 +0200

    added a function for accepting or declining mission

--- tvw_mission.py
+++ tvw_mission.py
@@ -8,8 +8,31 @@ Create a module with functions for the mission:
 - if yes: read file with description of the way and choices
 - function which short/long way
 """
-import tvw_charcreation 
+from tvw_charcreation import character_name
 
-with open("dwarf.txt", encoding='utf-8') as descr_file:
-        content = descr_file.read().replace("\n", " ")
-        return character_name, content
+# function for accepting mission or declining
+def mission():
+    print("*"*20)
+    with open("mission.txt", encoding='utf-8') as mission_file:
+        content = mission_file.read().replace("\n", " ")
+        print (content)
+    while True: #this creates an endless loop
+        print("."*20)
+        print(f"You have to save him, {character_name}! Please, will you help?")
+        save_Dayereth = input("To answer, please type yes or no: ")
+        if save_Dayereth == "yes":
+            print(f"'Thank you, {character_name}!' said Thara sighing in relief and overcomed by exhaustion, faints.")
+            print(f"Your expression shifts from calm to determined. You rise swiftly to your feet and make your way to the forest.")
+            break
+        elif save_Dayereth == "no":
+            print(f"'You are a terrible friend, {character_name}!' said Thara gasping in disbelief.")
+            break
+        else:
+            print(f"'I did not understand that, {character_name}!' Could you repeat?")
+    return save_Dayereth
+
+# function for outcome of the answer for the mission
+
+# function for the choice of the way
+
+mission()
