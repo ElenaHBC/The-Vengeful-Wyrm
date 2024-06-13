12f3377088032fd594e8747f5fa28c86293a3efc
Elena Tomeva <et@healthbiocare.at>
2024-06-13 12:06:21 +0200

    list of needed functions for the module

---
+++ DND_tvw.py
@@ -0,0 +1,13 @@
+""" Creating a custom mudule DND_tvw with the functions needed for the game.
+separate functions for:
+- choosing character from predefined list/dict while loop
+- giving name to the character while loop
+- learning details about the character if else + read file
+- saving the friend if else function
+- choosing a way while(if else)
+- short way riddle (like password func), 2 guesses while / or incorporate hangman style riddle
+- long way if else
+- initiative roll - separates for user and enemy?
+- attack roll - separates for user and enemy?
+- game function - while loop untill end
+"""
