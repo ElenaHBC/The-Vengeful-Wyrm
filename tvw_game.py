52b84119b983e96af3129ae2aaae55eed99fdc57
Elena Tomeva <et@healthbiocare.at>
2024-06-15 15:19:53 +0200

    function for initiation of the game

--- tvw_game.py
+++ tvw_game.py
@@ -1,3 +1,21 @@
-import random
-import tvw_charcreation
+"""
+Create a game function to:
+- give intro
+- initiate the game
+- go trough the story
+- rotate between attacks during battle
+- offer possibility to play again
+"""
 
+import random
+import tvw_charcreation, tvw_dices, tvw_mission
+
+play_again = input("Do you want to play again? yes/no: ")
+
+def the_vengeful_wyrm():
+    while play_again == "yes":
+        with open("intro.txt", encoding='utf-8') as intro_file:
+            content = intro_file.read().replace("\n", " ")
+            return content
+        
+the_vengeful_wyrm()
