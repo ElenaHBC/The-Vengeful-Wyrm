542cf9f5ab001292cfd3105bb92f2af4e1b77564
Elena Tomeva <et@healthbiocare.at>
2024-06-15 11:04:10 +0200

    corrected dice functions

--- tvw_dices.py
+++ tvw_dices.py
@@ -2,19 +2,56 @@
 Module for the different functions for the different dices
 - D20: for initiative, attack and skill checks
 - D6: for damage
+
+- separate function for rolling the dice and saving the outcome
+- separate functions for the user and enemy
 """
 
-import random
+from random import randint
 
+""" User's dices """
 # D6: dice with 6 sides
-def D_6():
-    d6 = random.randrange(1, 7)
-    return d6
+
+def user_D6():
+    while True:
+        roll = input("To roll a D6 dice type anything: ")
+        if roll:
+            user_d6 = randint(1, 6)
+            print(f"You rolled a {user_d6}!")
+            return user_d6
+        else:
+            print("You did not type anything, try again.")
+
+user_D6()
 
 # D20: dice with 20 sides
-def D_20():
-    d20 = random.randrange(1, 21)
-    return d20
+def user_D20():
+    while True:
+        roll = input("To roll a D20 dice type anything: ")
+        if roll:
+            user_d20 = randint(1, 20)
+            print(f"You rolled a {user_d20}!")
+            return user_d20
+        else:
+            print("You did not type anything, try again.")
 
-print(D_6())
-print(D_20())
+user_D20()
+
+
+""" Enemy's dices """
+# D6: dice with 6 sides
+
+def enemy_D6():
+    enemy_d6 = randint(1, 6)
+    print(f"The Wyrm rolled a {enemy_d6}!")
+    return enemy_d6
+
+enemy_D6()
+
+# D20: dice with 20 sides
+def enemy_D20():
+    enemy_d20 = randint(1, 20)
+    print(f"The Wyrm rolled a {enemy_d20}!")
+    return enemy_d20
+
+enemy_D20()
