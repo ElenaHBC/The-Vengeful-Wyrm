a652d604b155a965d12b026eef6f0b9ded646b85
Elena Tomeva <et@healthbiocare.at>
2024-06-17 15:45:32 +0200

    worked on the structure of the game, combining the modules

--- tvw_game.py
+++ tvw_game.py
@@ -7,23 +7,25 @@ Create a game function to:
 - offer possibility to play again
 """
 
-import random
-from tvw_charcreation import characters, choose_character, name_character, descr_character, game_intro
+from random import randint
+from tvw_charcreation import game_intro, characters, choose_character, name_character, descr_character
 from tvw_dices import user_D6, user_D20, enemy_D6, enemy_D20
 from tvw_mission import mission, mission_decision
 from tvw_path import choose_path, riddle, river
 
-play_again = input("Do you want to try again? yes/no: ")
+# defining some variables, so the code is more clean
 
 def the_vengeful_wyrm():
-    game_intro()
-    # defining some variables, so the code is more clean
+    print(game_intro())
+    print("Please choose a character from the following list: ")
+
     char_choice = choose_character(characters)
     character_name = name_character()
-    character_description = descr_character(char_choice)
+    character_description = descr_character(char_choice, character_name)
+    print(character_description)
     user = characters[char_choice]
-       
-    if mission() == "yes" or mission_decision(mission) == True:
+        
+    if mission() == "yes" or mission_decision(mission, character_name) == True:
         path = choose_path()
         if path == "wisdom":
             riddle()
@@ -32,13 +34,7 @@ def the_vengeful_wyrm():
 
     else:
         print("By declining the mission you decided not to play this game. This is disapointing.")
-            
-        
+        exit()
 
-    
-
-
-    while play_again == "yes": #this would be for playing again, but we need to initialize the first game
-       
         
 the_vengeful_wyrm()
