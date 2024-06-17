794f8bb5f7b95a1957550a83da283f7800e4ba19
Elena Tomeva <et@healthbiocare.at>
2024-06-17 12:21:06 +0200

    added the intro function to the gane

--- tvw_game.py
+++ tvw_game.py
@@ -8,27 +8,35 @@ Create a game function to:
 """
 
 import random
-from tvw_charcreation import characters, choose_character, name_character, char_choice
+from tvw_charcreation import characters, choose_character, name_character, descr_character, game_intro
 from tvw_dices import user_D6, user_D20, enemy_D6, enemy_D20
 from tvw_mission import mission, mission_decision
+from tvw_path import choose_path, riddle, river
 
-play_again = input("Do you want to play again? yes/no: ")
+play_again = input("Do you want to try again? yes/no: ")
 
 def the_vengeful_wyrm():
-    with open("intro.txt", encoding='utf-8') as intro_file: # starting the game with a short intro about it
-        content = intro_file.read().replace("\n", " ")
-        return content # code undearneath unreachable because of return statement 
-    
-    if mission() == "yes":
-        path = choose_path()
-    mission_decision()
-
-    
+    game_intro()
     # defining some variables, so the code is more clean
     char_choice = choose_character(characters)
     character_name = name_character()
     character_description = descr_character(char_choice)
     user = characters[char_choice]
+       
+    if mission() == "yes" or mission_decision(mission) == True:
+        path = choose_path()
+        if path == "wisdom":
+            riddle()
+        else:
+            river()
+
+    else:
+        print("By declining the mission you decided not to play this game. This is disapointing.")
+            
+        
+
+    
+
 
     while play_again == "yes": #this would be for playing again, but we need to initialize the first game
        
