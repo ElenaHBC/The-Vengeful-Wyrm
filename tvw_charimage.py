0576bfa33ef44fe712a215db561b3d6539ea4397
Elena Tomeva <et@healthbiocare.at>
2024-06-28 17:47:06 +0200

    module for fetching images from urls for character creation, random choice of 3 each

---
+++ tvw_charimage.py
@@ -0,0 +1,55 @@
+"""
+Module for fetching images for the characters through APIs
+- create a list of 3 urls for each race: dwarf, elf, human wizzard
+- each time a random image will be displayed after creating the character
+- inroduce the module and link it to the game
+"""
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import requests
+
+# Function to display image from URL
+def display_image(url):
+    try:
+        # Fetch the image
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+
+        # Open the image using PIL
+        img = Image.open(BytesIO(response.content))
+
+        # Display the image using matplotlib
+        plt.imshow(img)
+        plt.axis('off')  # Turn off axis numbers and ticks
+        plt.show()
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch image: {e}")
+    except IOError as e:
+        print(f"Failed to open image: {e}")
+
+# Example URL lists
+import random
+dwarf_img_list = ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f9e05a5d-03ce-4fb2-ba79-3b15306a0cdd/de5fzr2-4f1a02ab-d733-4186-9fba-bb810a622c88.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y5ZTA1YTVkLTAzY2UtNGZiMi1iYTc5LTNiMTUzMDZhMGNkZFwvZGU1ZnpyMi00ZjFhMDJhYi1kNzMzLTQxODYtOWZiYS1iYjgxMGE2MjJjODguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pYL1yabIDa1Cd7DOHV3-JXi5Z1yeBDm-LcxIo_qHZ20",
+              "https://i0.wp.com/nerdarchy.com/wp-content/uploads/2017/06/dwarfminer.jpg?w=564&ssl=1", 
+              "https://static.wikia.nocookie.net/forgottenrealms/images/b/b6/Dwarf-5e.png/revision/latest?cb=20180814005205"]
+dwarf_img = random.choice(dwarf_img_list)
+
+display_image(dwarf_img)
+
+elf_img_list = ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16839a88-ba93-4b12-a311-eded58cf9f7e/dg3at3m-d17dcd60-61de-45a1-99ca-1a6c120830ea.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2ODM5YTg4LWJhOTMtNGIxMi1hMzExLWVkZWQ1OGNmOWY3ZVwvZGczYXQzbS1kMTdkY2Q2MC02MWRlLTQ1YTEtOTljYS0xYTZjMTIwODMwZWEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TcPUWcZr8Sge3t25DMksvyG2-AzNhUbJaqdfzYvuq0Q",
+                "https://assets.playgroundai.com/091bb2df-428a-4f62-8062-a6533f714f6d.png",
+                "https://assets.playgroundai.com/35cf8f50-ca26-400c-983a-4ea76b04a217.png"]
+
+elf_img = random.choice(elf_img_list)
+
+display_image(elf_img)
+
+wizzard_img_list = ["https://images.nightcafe.studio/jobs/HVW6ec4BjYhrx4r5Z5Rx/HVW6ec4BjYhrx4r5Z5Rx--1--i6vxc.jpg?tr=w-1600,c-at_max",
+               "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3362b80d-a497-446e-91b3-89fa22a794a8/dgz379s-24246f54-c1fc-4259-a91e-5ccf90b7af75.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMzNjJiODBkLWE0OTctNDQ2ZS05MWIzLTg5ZmEyMmE3OTRhOFwvZGd6Mzc5cy0yNDI0NmY1NC1jMWZjLTQyNTktYTkxZS01Y2NmOTBiN2FmNzUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.N-YYmTwFJA7T8TZxcZpn8VJ4MPHYnYn3ZXO4Ab1wKb8",
+               "https://www.wargamer.com/wp-content/sites/wargamer/2022/01/dnd-wizard-5e-580x334.jpg"]
+
+wizzard_img = random.choice(wizzard_img_list)
+
+display_image(wizzard_img)
